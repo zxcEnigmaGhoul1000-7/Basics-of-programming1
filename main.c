db03d0ee41f62ed97e976636fbf646042099e725
zxcEnigmaGhoul1000-7 <maks2005430@gmail.com>
2024-06-07 11:34:31 +0300

    commit2

--- main.c
+++ main.c
@@ -8,24 +8,4 @@ int main() {
     printf("количество в последовательности: ");
     scanf("%d", &n);
 
-    int *posledovatelnost = (int *)malloc(n * sizeof(int));
-    int povtor[MAX_NUM] = {0};
-
-    printf("последовательность: ");
-    for (int i = 0; i < n; ++i) {
-        scanf("%d", &posledovatelnost[i]);
-        povtor[posledovatelnost[i]]++;
-    }
-
-    int oddCount = 0;
-    for (int i = 0; i < MAX_NUM; ++i) {
-        if (povtor[i] % 2 != 0) {
-            oddCount++;
-        }
-    }
-
-    printf("различные числа, нечетн раз: %d\n", oddCount);
-
-    free(posledovatelnost);
-    return 0;
-}
+    
